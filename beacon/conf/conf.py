f8ee0398e446b4199b69a492aa9e142af677d525
costero-e <orildsaga@gmail.com>
2024-12-05 09:07:43 +0100

    added configuration

--- beacon/conf/conf.py
+++ beacon/conf/conf.py
@@ -12,12 +12,12 @@ except Exception as e:# pragma: no cover
 
 level=logging.NOTSET
 log_file=None
-beacon_id = 'org.ega-archive.beacon-ri-demo'  # ID of the Beacon
-beacon_name = 'Beacon Reference Implementation demo'  # Name of the Beacon service
+beacon_id = 'org.ega-archive.ega-beacon'  # ID of the Beacon
+beacon_name = 'EGA Beacon'  # Name of the Beacon service
 api_version = 'v2.0.0' # Version of the Beacon implementation
-uri = 'http://beaconprod:5050/api/'
+uri = 'https://ega-beacon-demo.ega-archive.org/api/'
 environment = 'test'
-description = r"This Beacon is based on synthetic data hosted at the <a href='https://ega-archive.org/datasets/EGAD00001003338'>EGA</a>. The dataset contains 2504 samples including genetic data based on 1K Genomes data, and 76 individual attributes and phenotypic data derived from UKBiobank."
+description = r"This beacon is based on the European Genome-Phenome Archive datasets."
 version = api_version_yaml['api_version']
 welcome_url = 'https://beacon.ega-archive.org/'
 alternative_url = 'https://beacon.ega-archive.org/api'
